50ceb1674660717bb1e4839d5e8c7ac56d3911ec
aduxbury0 <aduxbury0@hotmail.co.uk>
2018-07-30 01:14:11 +0100

    added google auth

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    mongoURI:'mongodb://adux:a123456@ds231941.mlab.com:31941/storybooks',
+    googleClientID:'563869280008-t3calpjg7enpf2cufl5cp6n2kdoftal0.apps.googleusercontent.com',
+    googleClientSecret:'SUzHTiGO_Qgr6N6tFpdtRn9D'
+
+}
